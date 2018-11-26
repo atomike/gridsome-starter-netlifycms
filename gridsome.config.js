d9dddc5610a549843dcb8ff9a560dbdfe029045c
Michael Diez <mdiez@m10digital.com>
2018-11-25 21:33:50 -0500

    Added remark transformer to gridsome

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,22 +1,36 @@
 module.exports = {
-	siteName: 'Gridsome NetlifyCMS Starter Kit',
-	siteUrl: 'https://www.gridsome.org',
-	siteDescription: 'Gridsome is a blazing-fast static site generator...',
-	titleTemplate: `%s - Gridsome`,
-	plugins: [
-		{
-			use: '@gridsome/source-filesystem',
-			options: {
-				path: 'blog/*.md',
-				typeName: 'BlogPost',
-				route: '/blog/:slug'
-			}
-		},
-		// {
-		// 	use: '@gridsome/source-filesystem',
-		// 	options: {
-		// 		path: 'data/*.yml',
-		// 	}
-		// }
-	]
+  siteName: 'Gridsome NetlifyCMS Starter Kit',
+  siteUrl: 'https://www.gridsome.org',
+  siteDescription: 'Gridsome is a blazing-fast static site generator...',
+  titleTemplate: `%s - Gridsome`,
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+  plugins: [{
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/*.md',
+        typeName: 'BlogPost',
+        route: '/blog/:slug',
+        remark: {
+          plugins: [
+            // ... local plugins
+          ]
+        }
+      }
+    },
+    // {
+    // 	use: '@gridsome/source-filesystem',
+    // 	options: {
+    // 		path: 'data/*.yml',
+    // 	}
+    // }
+  ]
 }
